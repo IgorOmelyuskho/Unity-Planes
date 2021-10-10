b95d71866383320e2630422794d38db0065a94f4
Igor Omelyuskho <omelyushko.igor@gmail.com>
2021-10-10 12:55:30 +0300

    launck 2 rocket and some other

--- Assets/Scenes/rocketLauncher.cs
+++ Assets/Scenes/rocketLauncher.cs
@@ -9,8 +9,11 @@ public class rocketLauncher : MonoBehaviour
     public GameObject rocket3;
 
     public GameObject controlRocket;
+    public GameObject controlRocket2;
 
     public float speed;
+
+    public bool lookAtPlayer = false;
 
     // Start is called before the first frame update
     void Start()
@@ -30,15 +33,8 @@ public class rocketLauncher : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.X) && Shared.player)
         {
-            GameObject rocketClone = Instantiate(controlRocket, new Vector3(transform.position.x, transform.position.y, transform.position.z) - transform.up * 1.0f, transform.rotation);
-            rocketClone.GetComponent<controlObject>().isLaunchedRocket = true;
-            rocketClone.GetComponent<controlObject>().rb.velocity = Vector3.zero;
-            rocketClone.GetComponent<controlObject>().target = Shared.player;
-            //rocketClone.GetComponent<controlObject>().rocketOwner = gameObject;
-            Shared.player.GetComponent<controlObject>().addObjToInfoNearObjList(rocketClone);
-            Shared.hitWithBulletOrRocketObjects.Add(rocketClone);
-
-            Shared.lastLauncheInControlObjdRocket = rocketClone;
+            launchRocket(controlRocket);
+            launchRocket(controlRocket2);
 
             Shared.player.GetComponent<controlObject>().hp = 100;
         }
@@ -46,6 +42,23 @@ public class rocketLauncher : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (lookAtPlayer)
+        {
+            transform.LookAt(Shared.player.transform);
+        }
         /*transform.Translate(0, 0, speed);*/
+    }
+
+    void launchRocket(GameObject rocket)
+    {
+        GameObject rocketClone = Instantiate(rocket, new Vector3(transform.position.x, transform.position.y, transform.position.z) - transform.up * 1.0f, transform.rotation);
+        rocketClone.GetComponent<controlObject>().isLaunchedRocket = true;
+        rocketClone.GetComponent<controlObject>().rb.velocity = Vector3.zero;
+        rocketClone.GetComponent<controlObject>().target = Shared.player;
+        //rocketClone.GetComponent<controlObject>().rocketOwner = gameObject;
+        Shared.player.GetComponent<controlObject>().addObjToInfoNearObjList(rocketClone);
+        Shared.hitWithBulletOrRocketObjects.Add(rocketClone);
+
+        Shared.lastLauncheInControlObjdRocket = rocketClone;
     }
 }
